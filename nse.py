991a92282c93d788e6272beca59074800d929251
VISWA <66677784+VISWA22@users.noreply.github.com>
2022-04-08 15:46:02 +0530

    Update nse.py

--- nse.py
+++ nse.py
@@ -1,4 +1,5 @@
 print("Hello, world")
+sys.stdout.flush()
 from selenium import webdriver
 chrome_options = webdriver.ChromeOptions()
 chrome_options.add_argument("--no-sandbox")
@@ -8,5 +9,6 @@ browser = webdriver.Chrome(options=chrome_options)
 try:
     browser.get("www.nseindia.com/market-data/live-equity-market")
     print("Page title was '{}'".format(browser.title))
+    sys.stdout.flush()
 finally:
     browser.quit()
