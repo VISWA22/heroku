0ab698028c0129e170dc64e2bd23c65a1a993535
VISWA <66677784+VISWA22@users.noreply.github.com>
2022-04-08 15:42:02 +0530

    Update nse.py

--- nse.py
+++ nse.py
@@ -1,1 +1,12 @@
 print("Hello, world")
+from selenium import webdriver
+chrome_options = webdriver.ChromeOptions()
+chrome_options.add_argument("--no-sandbox")
+chrome_options.add_argument("--headless")
+chrome_options.add_argument("--disable-gpu")
+browser = webdriver.Chrome(options=chrome_options)
+try:
+    browser.get("www.nseindia.com/market-data/live-equity-market")
+    print("Page title was '{}'".format(browser.title))
+finally:
+    browser.quit()
