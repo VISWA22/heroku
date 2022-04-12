f7de755b8b434a0c01bbaa06ed871146724bc839
VISWA <66677784+VISWA22@users.noreply.github.com>
2022-04-12 14:34:20 +0530

    Update nse.py

--- nse.py
+++ nse.py
@@ -1,8 +1,7 @@
 import sys
-import os
+import requests
 print("Hello, world")
 sys.stdout.flush()
-import requests
 url = "https://www.nseindia.com/"
 headers = {'user-agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.117 Safari/537.36'}
 response = requests.get(url, headers=headers)
