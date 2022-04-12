634249c9d797a5c3f4bd555f0b0474e4a9bc6ccf
VISWA <66677784+VISWA22@users.noreply.github.com>
2022-04-12 15:24:04 +0530

    Update nse.py

--- nse.py
+++ nse.py
@@ -2,7 +2,7 @@ import sys
 import requests
 print("Hello, world")
 sys.stdout.flush()
-url = "https://www.nseindia.com/market-data/live-equity-market?symbol=NIFTY%2050"
+url = "https://stockezee.com/nse-fno-lot-size"
 headers = {'user-agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.117 Safari/537.36'}
 response = requests.get(url, headers=headers)
 print(response)
