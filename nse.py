def1e817c6a9ba762a2915ee8bcbdc62b15ac067
VISWA <66677784+VISWA22@users.noreply.github.com>
2022-04-12 14:52:59 +0530

    Update nse.py

--- nse.py
+++ nse.py
@@ -2,7 +2,7 @@ import sys
 import requests
 print("Hello, world")
 sys.stdout.flush()
-url = "https://stackoverflow.com/questions/15767685/understanding-heroku-server-status-143"
+url = "https://www.nseindia.com/market-data/live-equity-market"
 headers = {'user-agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.117 Safari/537.36'}
 response = requests.get(url, headers=headers)
 print(response)
