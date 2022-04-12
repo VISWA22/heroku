89f8c836a2c8aeb1f1b0893ba725246e5f5df146
VISWA <66677784+VISWA22@users.noreply.github.com>
2022-04-12 14:09:45 +0530

    Update nse.py

--- nse.py
+++ nse.py
@@ -11,7 +11,7 @@ chrome_options.add_argument("--headless")
 chrome_options.add_argument("--disable-dev-shm-usage")
 chrome_options.add_argument("--no-sandbox")
 user_agent = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/33.0.1750.517 Safari/537.36'
-chrome_options.add_argument('user-agent={0}'.format(user_agent))
+chrome_options.add_argument('--user-agent={0}'.format(user_agent))
 browser = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), options=chrome_options)
 #wait = WebDriverWait(driver, 20)
 #action = ActionChains(driver)
