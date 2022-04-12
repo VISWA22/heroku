a5d43629c064e5c21ef93a58d0b90e3745818ddb
VISWA <66677784+VISWA22@users.noreply.github.com>
2022-04-12 18:35:22 +0530

    Update nse.py

--- nse.py
+++ nse.py
@@ -2,8 +2,8 @@ import sys
 import requests
 print("Hello, world")
 sys.stdout.flush()
-url = "https://stockezee.com/nse-fno-lot-size"
-headers = {'user-agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.117 Safari/537.36'}
+url = "https://www.nseindia.com/api/equity-stockIndices?index=SECURITIES%20IN%20F&O"
+headers = {'user-agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.75 Safari/537.36 Edg/100.0.1185.36'}
 response = requests.get(url, headers=headers)
 print(response)
 sys.stdout.flush()
