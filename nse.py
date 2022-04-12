e2eedd94b3fc032c9a7bc6b9d01c4a7b29fe9c7d
VISWA <66677784+VISWA22@users.noreply.github.com>
2022-04-12 19:13:41 +0530

    Update nse.py

--- nse.py
+++ nse.py
@@ -1,8 +1,11 @@
 import sys
 import os
+import time
+from selenium import webdriver
+
 print("Hello, world")
 sys.stdout.flush()
-from selenium import webdriver
+
 chrome_options = webdriver.ChromeOptions()
 chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
 chrome_options.add_argument("--headless")
@@ -12,7 +15,7 @@ user_agent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTM
 chrome_options.add_argument('--user-agent={0}'.format(user_agent))
 browser = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), options=chrome_options)
 browser.get("https://www.nseindia.com/api/equity-stockIndices?index=SECURITIES%20IN%20F&O")
-print((browser.title))
-sys.stdout.flush()
+time.sleep(3)
+driver.find_element(By.LINK_TEXT,"Download (.csv)").click()
 browser.quit()
     
