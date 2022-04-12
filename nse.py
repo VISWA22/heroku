b4645ef2e61de6756e8f675a5bb58ab6a58f933c
VISWA <66677784+VISWA22@users.noreply.github.com>
2022-04-12 14:25:01 +0530

    Update nse.py

--- nse.py
+++ nse.py
@@ -5,7 +5,7 @@ sys.stdout.flush()
 import requests
 url = "https://www.nseindia.com/"
 headers = {'user-agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.117 Safari/537.36'}
-response = requests.get(stock_url, headers=headers)
+response = requests.get(url, headers=headers)
 print(response)
 sys.stdout.flush()
 
