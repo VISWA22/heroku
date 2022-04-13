37d80c070f8f7a2428cce5004733e62e4f9c9c44
VISWA <66677784+VISWA22@users.noreply.github.com>
2022-04-13 15:23:53 +0530

    Update nse.py

--- nse.py
+++ nse.py
@@ -16,7 +16,7 @@ chrome_options.add_argument('--user-agent={0}'.format(user_agent))
 browser = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), options=chrome_options)
 browser.get("https://www.browserstack.com/test-on-the-right-mobile-devices")
 time.sleep(3)
-gotit= driver.find_element_by_id('accept-cookie-notification');
+gotit= browser.find_element_by_id('accept-cookie-notification');
 gotit.click();
 button = browser.find_element_by_css_selector('.icon-csv')
 button.click()
